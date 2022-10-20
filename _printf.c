63171074ec04d9208479ea7a296b460efa5a4871
erokemwa <erokemwa@yahoo.com>
2022-10-20 12:29:15 -0700

    Printf

--- _printf.c
+++ _printf.c
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-
 #include "main.h"
-
-
-=======
-#include "main.h"
->>>>>>> e76c6638b30d2309b8c9c163f5b6a074ba37c3b6
 
 /**
  *_printf - prints to the output
@@ -16,30 +9,16 @@
  *
  */
 
-
-
 int _printf(const char *format, ...)
-
 {
-
 	va_list arg;
-
 	int done;
-
-
 
 	va_start(arg, format);
 
-
-
 	done = vfprintf(stdout, format, arg);
-
 	va_end(arg);
-
-
-
+		
 	return (done);
-
-
 
 }
